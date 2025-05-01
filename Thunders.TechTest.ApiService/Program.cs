e6b4c157c4e3a5740b7de6cde74e3e6429465579
Marcelo Morais <marceloms17@gmail.com>
2025-05-01 16:05:10 -0300

    Ajustes no projeto, referencias e arquivos da solution

--- Thunders.TechTest.ApiService/Program.cs
+++ Thunders.TechTest.ApiService/Program.cs
@@ -1,12 +1,21 @@
 using Microsoft.EntityFrameworkCore;
+using OpenTelemetry.Trace;
 using Thunders.TechTest.ApiService;
+using Thunders.TechTest.ApiService.Application.Commands;
+using Thunders.TechTest.ApiService.Application.Messages;
+using Thunders.TechTest.ApiService.Interfaces;
+using Thunders.TechTest.ApiService.Middlewares;
+using Thunders.TechTest.ApiService.Services;
 using Thunders.TechTest.OutOfBox.Database;
 using Thunders.TechTest.OutOfBox.Queues;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.AddServiceDefaults();
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.PropertyNameCaseInsensitive = true;
+});
 
 var features = Features.BindFromConfiguration(builder.Configuration);
 
@@ -15,22 +24,47 @@ builder.Services.AddProblemDetails();
 
 if (features.UseMessageBroker)
 {
-    builder.Services.AddBus(builder.Configuration, new SubscriptionBuilder());
+    builder.Services.AddBus(builder.Configuration, new SubscriptionBuilder()
+         .Add<RegisterTollUsageMessage>());
+
+    builder.Services.AddScoped<IMessageSender, RebusMessageSender>();
 }
 
 if (features.UseEntityFramework)
 {
-    builder.Services.AddSqlServerDbContext<DbContext>(builder.Configuration);
+    builder.Services.AddSqlServerDbContext<AppDbContext>(builder.Configuration);
+    builder.Services.AddScoped<ITollUsageRepository, TollUsageRepository>();
 }
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(EnqueueTollUsageCommand).Assembly));
+builder.Services.AddOpenTelemetry()
+    .WithTracing(tracing =>
+    {
+        tracing
+            .AddAspNetCoreInstrumentation()
+            .AddHttpClientInstrumentation()
+            .AddEntityFrameworkCoreInstrumentation();
+    });
 
 var app = builder.Build();
 
+if (features.UseEntityFramework)
+{
+    using var scope = app.Services.CreateScope();
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
 // Configure the HTTP request pipeline.
-app.UseExceptionHandler();
+app.UseMiddleware<ErrorHandlingMiddleware>();
 
 app.MapDefaultEndpoints();
 
 app.MapControllers();
+app.UseSwagger();
+app.UseSwaggerUI();
+app.UseDeveloperExceptionPage();
 
 app.Run();
